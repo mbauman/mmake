b07d3eb17fee9859de279c471a9ad3a8f0885888
Matt Bauman <mbauman@gmail.com>
2011-04-30 09:24:58 -0400

    Add licensing information and README

--- mmake.m
+++ mmake.m
@@ -22,7 +22,7 @@ function mmake(varargin)
 %           - $* = the pattern matched by '%' in a pattern rule
 %           - $& = the directory of the target
 %       - MATLAB command expansion via $(eval cmd) or ${eval cmd}. The
-%           string 'cmd' is evaluated directly within MATLAB.  It must return a
+%           string 'cmd' is evaluated directly within MATLAB. It must return a
 %           value of type char or a cell array of chars. In the event that a
 %           multidimensional array or cell array is returned, all elements are
 %           concatenated together with a space in between.
@@ -45,8 +45,30 @@ function mmake(varargin)
 %   implicit rules. The optional second argument may be used to specify a
 %   MMakefile in another directory or saved as a different name.
 %
-%   Matt Bauman, 2010. mbauman@gmail.com.
+%   Copyright (c) 2011, Matt Bauman. mbauman@gmail.com.
+%   All rights reserved.
 
+%   Redistribution and use in source and binary forms, with or without
+%   modification, are permitted provided that the following conditions are
+%   met:
+%
+%   - Redistributions of source code must retain the above copyright notice,
+%     this list of conditions and the following disclaimer.
+%   - Redistributions in binary form must reproduce the above copyright
+%     notice, this list of conditions and the following disclaimer in the
+%     documentation and/or other materials provided with the distribution.
+%
+%   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+%   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+%   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+%   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+%   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+%   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+%   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+%   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+%   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+%   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+%   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 %% Argument parsing and setup
 wd = '';
