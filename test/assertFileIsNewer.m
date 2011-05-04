4456d497e1bf939db33ef1b09f643f7aac337f6a
Matt Bauman <mbauman@gmail.com>
2011-05-03 23:10:49 -0400

    Add sensible errors for when files don't exist

--- test/assertFileIsNewer.m
+++ test/assertFileIsNewer.m
@@ -4,4 +4,6 @@ function assertFileIsNewer(fileA,fileB)
 a = dir(fileA);
 b = dir(fileB);
 
+assert(~isempty(a),'%s should exist but it doesn''t',fileA);
+assert(~isempty(b),'%s should exist but it doesn''t',fileB);
 assert(a.datenum >= b.datenum,'%s is not newer than %s', fileA, fileB);
